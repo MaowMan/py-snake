5094b61798ad54f7af080a4dad33afad1f39a6ee
MaowMan <54308739+MaowMan@users.noreply.github.com>
2019-12-17 22:53:16 +0800

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,94 +1,95 @@
 from tkinter import *
-import tkinter.font as tkFont
+from tkinter import messagebox
+import tkinter.font as tkfont
+import yaml
 from random import randint
-class main():
+class main(object):
     def __init__(self):
+        with open("config.yml","r",encoding="utf-8") as reader:
+            self.config=yaml.load(reader,Loader=yaml.SafeLoader)
         self.root=Tk()
         self.setup()
         self.widget()
-        self.snake_init()
-        self.setup_bind()
+        self.snake()
         self.loop()
     def setup(self):
-        self.root.title("Python貪食蛇")
-        self.w=32
-        self.h=32
-        self.ms=20
-        self.fs=2000
-        self.lenth=24
-        self.fruits=[]
-        self.font=tkFont.Font(family="helvetica",size=6)
+        self.w=self.config["game"]["width"]
+        self.h=self.config["game"]["height"]
+        self.speed=self.config["game"]["speed"]
+        self.lenth=self.config["game"]["lenth"]
+        self.fruitscore=self.config["game"]["fruitscore"]
+        self.fruitfreq=self.config["game"]["fruitfreq"]
+        self.scoreVar=IntVar(0)
+        self.font=tkfont.Font(family=self.config["word"]["font"],size=self.config["word"]["size"])
     def widget(self):
-        self.pxs=[]
+        self.root.title(self.config["word"]["title"])
+        self.labels=[]
         for y in range(self.h):
-            self.pxs.append([])
+            self.labels.append([])
             for x in range(self.w):
-                self.pxs[-1].append(Label(self.root,borderwidth=1,fg="white",bg="white",text="O",relief="solid",font=self.font))
-                self.pxs[-1][-1].grid(row=y+1,column=x+1)
-    def snake_init(self):
-        self.direction=(0,1)
+                self.labels[y].append(Label(self.root,text=self.config["word"]["intile"],font=self.font,borderwidth=1,relief="solid"))
+                self.change_color((y,x),"nothing")
+                self.labels[y][x].grid(row=y,column=x)
+                #print("({},{})".format(y,x))
+    def snake(self):
         self.head=(self.h//2,self.w//2)
-        self.body=[self.head]
-        for i in range(self.lenth-1):
-            self.body.append((self.head[0]+self.direction[0]*-1,self.head[1]+self.direction[1]*-1))
-    def setup_bind(self):
-        self.root.bind("<w>",lambda x: self.change_direction("w"))
-        self.root.bind("<s>",lambda x: self.change_direction("s"))
-        self.root.bind("<a>",lambda x: self.change_direction("a"))
-        self.root.bind("<d>",lambda x: self.change_direction("d"))
+        self.body=[]
+        self.fruits=[]
+        self.direction=(0,1)
+        self.direction_flag=True
+    def loop(self):
+        self.root.bind("<w>",lambda x: self.change_direction((-1,0)))
+        self.root.bind("<s>",lambda x: self.change_direction((1,0)))
+        self.root.bind("<a>",lambda x: self.change_direction((0,-1)))
+        self.root.bind("<d>",lambda x: self.change_direction((0,1)))
+        self.root.after(self.fruitfreq,self.genfruit)
+        self.root.after(self.speed,self.update)
+        self.root.mainloop()
     def update(self):
         self.head=(self.head[0]+self.direction[0],self.head[1]+self.direction[1])
+        self.direction_flag=True
+        #print(self.head)
         try:
-            self.change_px(self.head,"black")
+            self.change_color(self.head,"snake")
         except (IndexError):
-            if self.direction==(0,1):
-                self.head=(self.head[0],0)
-            elif self.direction==(0,-1):
-                self.head=(self.head[0],self.w-1)
-            elif self.direction==(1,0):
-                self.head=(0,self.head[1])
-            elif self.direction==(-1,0):
-                self.head=(self.h-1,self.head[1])
-            self.change_px(self.head,"black")
+            self.gameover()
         if self.head in self.body:
-            index=self.body.index(self.head)
-            for element in self.body[index:]:
-                self.change_px(element,"white")
-            self.body=self.body[0:index]
-            self.lenth=len(self.body)
-        if self.head in self.fruits:
-            print("+")
-            self.lenth+=5
-            self.fruits.remove(self.head)
-        self.change_px(self.head,"black")
+            self.gameover()
         self.body.insert(0,self.head)
         if len(self.body)>self.lenth:
-            if self.body[-1] not in self.fruits:
-                self.change_px(self.body[-1],"white")
-            self.body.pop()
-        self.root.after(self.ms,self.update)
-    def change_direction(self,char):
-        if char=="w" and self.direction!=(1,0):
-            self.direction=(-1,0)
-        elif char=="s" and self.direction!=(-1,0):
-            self.direction=(1,0)
-        elif char=="a" and self.direction!=(0,1):
-            self.direction=(0,-1)
-        elif char=="d" and self.direction!=(0,-1):
-            self.direction=(0,1)
-    def generate_fruit(self):
+            self.change_color(self.body[-1],"nothing")
+            del self.body[-1]
+        if self.head in self.fruits:
+            self.lenth+=self.fruitscore
+            self.fruits.remove(self.head)
+        self.root.after(self.speed,self.update)
+    def change_color(self,coords,state):
+        if coords[0]<0 or coords[1]<0:
+            raise IndexError
+        label=self.labels[coords[0]][coords[1]]
+        color=self.config["color"][state]
+        try:
+            if label.snake_state=="fruit":
+                if state=="snake":
+                    label.config(fg=color,bg=color)
+            else:
+                label.config(fg=color,bg=color)
+        except (AttributeError):
+            label.snake_state=state
+            label.config(fg=color,bg=color)
+    def genfruit(self):
         fruit=(randint(0,self.h-1),randint(0,self.w-1))
         while fruit in self.fruits:
             fruit=(randint(0,self.h-1),randint(0,self.w-1))
-        self.change_px(fruit,"red")
         self.fruits.append(fruit)
-        self.root.after(self.fs,self.generate_fruit)
-    def loop(self):
-        self.root.after(self.ms,self.update)
-        self.root.after(self.fs,self.generate_fruit)
-        self.root.mainloop()
-    def change_px(self,coords,color):
-        self.pxs[coords[0]][coords[1]].config(fg=color,bg=color)
+        self.change_color(fruit,"fruit")
+        self.root.after(self.fruitfreq,self.genfruit)
+    def change_direction(self,direction):
+        if direction != (self.direction[0]*-1,self.direction[1]*-1) and self.direction_flag==True:
+            self.direction=direction
+            self.direction_flag=False
+    def gameover(self):
+        messagebox.showinfo(self.config["word"]["title"],self.config["word"]["end"])
+        self.root.quit()
 if __name__=="__main__":
     main()
-
